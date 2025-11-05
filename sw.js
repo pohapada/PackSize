521116b4721b6b14c9912d87c345de4f56ee6c08
Mohamad Amin <pohapada@outlook.com>
2025-11-05 15:33:35 +0330

    Update and rename service-worker.js to sw.js

---
+++ sw.js
@@ -0,0 +1,55 @@
+// فایل: sw.js
+const CACHE_NAME = 'carton-calculator-v3';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installing');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => {
+        console.log('All resources cached successfully');
+      })
+      .catch(error => {
+        console.log('Cache installation failed:', error);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activated');
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
